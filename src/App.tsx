5d15ec4b4bc96a0c819b490dfb13d826c5debfb1
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-28 11:55:51 +0900

    Practice stiffness, damping , bounce, duration

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,20 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: tomato;
 `;
 
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 function App() {
   return (
     <Wrapper>
-      <Box animate={{ borderRadius: 100 }}>Box</Box>
+      <Box transition={{ type: 'spring', bounce: 0.85 }} initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360 }} />
     </Wrapper>
   );
 }
