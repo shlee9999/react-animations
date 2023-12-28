71e73a4be14b04b51e567192e5f99e43e201c9d5
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-28 14:46:48 +0900

    Practice properties about Drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { motion, Variants } from 'framer-motion';
+import { useRef } from 'react';
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -12,7 +13,7 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255);
   border-radius: 15px;
   box-shadow:
     0 2px 3px rgba(0, 0, 0, 0.1),
@@ -20,75 +21,50 @@ const Box = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
 `;
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  background-color: white;
-  box-shadow:
-    0 2px 3px rgba(0, 0, 0, 0.1),
-    0 10px 20px rgba(0, 0, 0, 0.06);
-  border-radius: 100%;
-  place-self: center; // 공부
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
-// const boxVariants: Variants = {
-//   start: {
-//     opacity: 0,
-//     scale: 0.5,
-//   },
-//   end: {
-//     opacity: 1,
-//     scale: 1,
-//     transition: {
-//       type: 'spring',
-//       duration: 1,
-//       bounce: 0.5,
-//       delayChildren: 0.5,
-//       staggerChildren: 0.2,
-//     },
-//   },
-// };
-
-const circleVariatns: Variants = {
-  start: {
-    opacity: 0,
-    y: 10,
+const boxVariants: Variants = {
+  hover: {
+    rotateZ: 90,
   },
-  end: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      bounce: 0.8,
-      duration: 1,
-    },
-  },
-};
-const boxVariants = {
-  // nico source
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
+  click: {
     scale: 1,
-    opacity: 1,
-    transition: {
-      type: 'spring',
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
+    borderRadius: '100px',
+  },
+  drag: {
+    backgroundColor: 'rgb(76, 209, 55)',
+    // transition: {
+    //   duration: 0.5,
+    //   bounce: 0.5,
+    // },
   },
 };
+
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariatns} />
-        <Circle variants={circleVariatns} />
-        <Circle variants={circleVariatns} />
-        <Circle variants={circleVariatns} />
-      </Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag"
+          dragConstraints={biggerBoxRef}
+          dragSnapToOrigin
+          dragElastic={1}
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
