8b747c48414c23fe35dcb53f0dede9acbcd1e4a9
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-28 11:29:28 +0900

    Install Recoil, Framer-Motion

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { RecoilRoot } from 'recoil';
+import { darkTheme } from './theme';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
